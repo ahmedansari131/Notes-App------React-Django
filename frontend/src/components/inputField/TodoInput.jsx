9237db937544e7d14b3ee4d6d966388eea87007b
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-09-25 20:39:39 +0530

    Changed in the style

--- frontend/src/components/inputField/TodoInput.jsx
+++ frontend/src/components/inputField/TodoInput.jsx
@@ -1,30 +1,16 @@
-import React, { useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setTodoInputFocus,
-  setTodoDescInputActive,
-  setTodoTitleInputActive,
-} from "../../slices/todoInputSlice";
-import { setTodoData } from "../../slices/todoSlice";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setTodoInputFocus } from "../../slices/todoInputSlice";
 
 const TodoInput = (props) => {
   const { placeholder, action, inputReference } = props;
-  const inputRef = useRef();
   const dispatch = useDispatch();
-  const setTodoDescInputActive = useSelector(
-    (state) => state.todoInput.todoDescInput
-  );
-  const setTodoTitleInputActive = useSelector(
-    (state) => state.todoInput.todoTitleInput
-  );
 
   return (
     <div className="flex flex-col w-full items-start gap-4 relative">
-      {/* {(!setTodoDescInputActive || !setTodoTitleInputActive) ? ( */}
-        <div className="absolute top-1/2 -translate-y-1/2 text-sm pl-4 font-medium selection:bg-none pointer-events-none">
-          {placeholder}
-        </div>
-    {/* //   ) : ""} */}
+      <div className="absolute top-1/2 -translate-y-1/2 text-sm pl-4 font-medium selection:bg-none pointer-events-none">
+        {placeholder}
+      </div>
       <div
         ref={inputReference}
         contentEditable={true}
