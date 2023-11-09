615c188092594f283d770fe7c6354a87f32d78f8
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-11-09 14:06:30 +0530

    endpoints created for the fetching the single note

--- backend/crud/urls.py
+++ backend/crud/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import FetchTodo, CreateTodo, DeleteTodo, UpdateTodo
 
 urlpatterns = [
     path('', FetchTodo.as_view(), name="todolist"),
-    path('createtodo/', CreateTodo.as_view(), name="createtodo"),
+    path('<int:id>/', FetchTodo.as_view(), name="note"),
+    path('createnote/', CreateTodo.as_view(), name="createNote"),
     path('deletetodo/<int:pk>/', DeleteTodo.as_view(), name="deletetodo"),
     path('updatetodo/<int:pk>/', UpdateTodo.as_view(), name="updatetodo"),
 ]
