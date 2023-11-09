77f789242140744ae8298a3eb301aa7726a50b8e
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-11-09 14:02:30 +0530

    added reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../slices/todoSlice";
 import { crudApi } from "../services/crudApi";
-import todoInputSlice from "../slices/todoInputSlice";
-import dropdownSlice from "../slices/dropdownSlice";
+import updateNoteSlice from "./slices/updateNote/updateNoteSlice";
+import notesSlice from "./slices/noteData/notesSlice";
+import dropdownSlice from "./slices/dropdown/dropdownSlice";
 
 export const store = configureStore({
     reducer: {
-        todo: todoReducer,
-        todoInput: todoInputSlice,
+        updateNote: updateNoteSlice,
+        notes: notesSlice,
         dropdown: dropdownSlice,
         [crudApi.reducerPath]: crudApi.reducer,
     },
